ff94dc2265a4efb0ec00d88ab9283ccaafe18b18
Patrick Brown <patrick.barry.brown@gmail.com>
2024-12-02 22:15:39 -0700

    day3 solution day4 scaffold

--- day3/day3.py
+++ day3/day3.py
@@ -1,18 +1,36 @@
-
+import re
 
 def p1():
   with open("input.txt", "r") as file:
-      for line in file:
-        items = line.strip().split()
+      data = file.read()
         
+  matches = re.findall(r'mul\(\d+,\d+\)', data)
 
+  items = []
+  for match in matches:
+    a = match[4:-1].split(',')
+    items.append(int(a[0]) * int(a[1]))
 
-
+  print(sum(items))
 
 def p2():
   with open("input.txt", "r") as file:
-      for line in file:
-        items = line.strip().split()
+      data = file.read()
+
+  matches = re.findall(r'mul\(\d+,\d+\)|don\'t|do', data)
+
+  enabled = True
+  items = []
+  for match in matches:
+    if match == "do":
+      enabled = True
+    elif match == "don't":
+      enabled = False
+    elif enabled == True:
+      a = match[4:-1].split(',')
+      items.append(int(a[0]) * int(a[1]))
+
+  print(sum(items))
 
 if __name__ == "__main__":
-  p1()
+  p2()
---
+++ day4/day4.py
@@ -0,0 +1,13 @@
+import re
+
+def p1():
+  with open("input.txt", "r") as file:
+      for line in file:
+        items = line.strip().split()
+
+def p2():
+  with open("input.txt", "r") as file:
+      data = file.read()
+
+if __name__ == "__main__":
+  p1()
